9351c19a585fdb2f2d55eaf0c041469b56c55280
Shahadat_Robin <robin@corexlab.dev>
2023-01-03 15:53:50 +0600

    inputs added

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
 
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       
-      <Text style={styles.dummyText}>Hello there!!!</Text>
-      <Button title='Click Mee'/>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Enter your Name'/>
+        <Button title='Add Name'/>
+      </View>
+      <View style={styles.nameContainer}>
+        <Text>Name List..</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText:{
-    color: 'blue',
-    backgroundColor: 'white',
-    borderWidth: 2,
-    padding: 16,
-    margin:16,
-    borderColor:'red',
-  }
+  
+appContainer:{
+paddingTop:50,
+paddingHorizontal:16,
+flex:1
+},
+inputContainer:{
+  flex:1,
+  flexDirection:'row',
+  justifyContent: 'space-between',
+  alignItems:'center',
+  borderBottomWidth: 1,
+  borderColor: '#ccc',
+  marginBottom:24
+  
+},
+textInput:{
+  width: '70%',
+  borderWidth:1,
+  borderColor: '#ccc',
+  marginRight:8,
+  padding:8
+  
+},
+nameContainer:{
+flex:5,
+}
 });
